b99543632c0266acb28709043e2438f01847c2a7
phantom0174 <darrenhsiou@gmail.com>
2022-01-29 10:25:19 +0800

    create role command

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Etapocsm
+
+<img src="https://i.imgur.com/MrSR8kJ.jpg" alt="icon" width=400>
+
+## 🔧 Developer
+
+- Programed and run by `@phantom0174`, made with ❤️️.
+
+## ☘️ Introduction
+
+- A new bot as a alternative of SQCS_Bot (which has shut down due to discord.py outage and bot maintenance), written in JS using official Discord API.
--- bot.js
+++ bot.js
@@ -14,54 +14,87 @@ bot.on('ready', () => {
     const guildId = '790978307235512360';
     const guild = bot.guilds.cache.get(guildId);
 
-    let commands
-    if (guild) {
-        commands = guild.commands;
-    } else {
-        commands = bot.application?.commands;
-    }
+    let commands = guild.commands;
 
-    commands?.create({
+    commands.create({
         name: 'ping',
-        description: 'Hit the bot!',
+        description: 'Hit the bot!'
     });
 
-    commands?.create({
+    commands.create({
         name: 'get_cadre_role',
-        description: 'get cadre role',
+        description: '獲取幹部身分組',
         options: [
             {
                 name: 'role',
-                description: 'see billboard',
-                required: true,
+                description: '你要獲取的幹部身分組',
                 type: Constants.ApplicationCommandOptionTypes.STRING,
+                required: true,
+                choices: [
+                    {
+                        name: '副召',
+                        value: '790978959411904532'
+                    },
+                    {
+                        name: '學術',
+                        value: '828317945796231178'
+                    },
+                    {
+                        name: '網管',
+                        value: '790979485779886112'
+                    },
+                    {
+                        name: '公關',
+                        value: '790979506906726471'
+                    },
+                    {
+                        name: '美宣',
+                        value: '790979546785775626'
+                    },
+                    {
+                        name: '議程',
+                        value: '790979524090658857'
+                    }
+                ]
             }
-        ],
-    })
+        ]
+    });
 });
 
 bot.on('interactionCreate', async (interaction) => {
+    const working_guild_id = '790978307235512360';
+    if (interaction.guildId !== working_guild_id) return;
+
     if (!interaction.isCommand()) {
         return;
     }
-    const { commandName, options } = interaction;
 
-    if (commandName === 'ping') {
+    if (interaction.commandName === 'ping') {
         interaction.reply({
             content: 'pong!',
-            ephemeral: false,
+            ephemeral: false
         });
     }
-});
 
-bot.on('messageCreate', (message) => {
-    if (message.guildId !== '790978307235512360') {
-        return;
-    }
+    if (interaction.commandName === 'get_cadre_role') {
+        const role_token_id = '791680285464199198';
 
-    if (message.content === '%ping') {
-        message.reply({
-            content: 'Pong!'
+        if (!interaction.member.roles.cache.some(role => role.id === role_token_id)) {
+            interaction.reply({
+                content: '請求拒絕，你沒有 `role-token` 身分組呦，詳情請洽總召。',
+                ephemeral: true
+            });
+            return;
+        }
+
+        const role_id = interaction.options.getString('role');
+
+        interaction.member.roles.add(role_id);
+        interaction.member.roles.remove(role_token_id);
+
+        interaction.reply({
+            content: '幹部身分組已給予，請察收！',
+            ephemeral: true
         })
     }
 });
